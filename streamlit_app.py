a82afd23abd517c8e418fbd9c16ab06ac44018ba
daniel-dabbah <115877882+daniel-dabbah@users.noreply.github.com>
2026-04-22 09:22:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,27 @@
 import streamlit as st
-st.title("Hello Streamlit-er 👋")
 
-if st.button("לחץ עליי"):
-    st.write("לחצת על הכפתור 🎉")
+st.title("בודק מצב רוח 😄")
 
+# בחירת מצב רוח
+mood = st.selectbox(
+    "איך אתה מרגיש היום?",
+    ["שמח 😄", "עייף 😴", "לחוץ 😰"]
+)
 
+# בחירת אנרגיה
+energy = st.slider("כמה אנרגיה יש לך?", 0, 10)
+
+# כפתור
+if st.button("תן לי המלצה"):
+    
+    if mood == "שמח 😄":
+        if energy > 5:
+            st.write("לך לבלות עם חברים 🎉")
+        else:
+            st.write("תשמע מוזיקה טובה 🎧")
+    
+    elif mood == "עייף 😴":
+        st.write("לך לישון קצת 😴")
+    
+    elif mood == "לחוץ 😰":
+        st.write("קח נשימה עמוקה 🧘‍♂️")
